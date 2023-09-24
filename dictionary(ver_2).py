e2b6d98540af9633a57105c931d90df54e48f45a
朝比奈 <138207613+Asahina118@users.noreply.github.com>
2023-09-24 16:12:40 +0800

    Add files via upload

---
+++ dictionary(ver_2).py
@@ -0,0 +1,69 @@
+import random
+select =''
+
+while select != 'Q':
+    hist = []
+    li = ''
+    with open('漢字と意味.txt',encoding='utf8') as fp:
+        for i, l in enumerate(fp):
+            pass
+    num = i+1
+    print('words contained in the file:',num)
+    fp.close()
+    select = input('Select(D/A/L/Q):')
+    if select == 'D':
+        print('enter *quit* to exit dictation') 
+
+    while select == "D":
+        if li == '':
+            li = input('Display correct list throughout the dictation?(Y/N)')
+        file_choice = input('File version(1/2):')
+        file_choice = ('漢字と意味' + file_choice + '.txt')
+        rep = True
+        while (rep):
+            rep = False
+            file = open(file_choice,'r',encoding='utf8')
+            line = next(file)
+            for num, aline in enumerate(file, 2):
+                if random.randrange(num):
+                    continue
+                line = aline
+            temp_list = line.split()
+            for i in range(len(hist)):
+                if temp_list[1] == hist[i]:
+                    rep = True
+        print(temp_list[1])
+        x = input("Answer:")
+        while x != temp_list[0] and x!='quit':
+            print("Incorrect")
+            x = input("Again:")
+        if x == 'quit':
+            break
+        if x == temp_list[0]:
+            print("Correct!")
+            hist.append(temp_list[1])
+            if li == 'Y':
+                print('words got right:',hist)
+        if len(hist) == num:
+            print('all words are dictated')
+            break
+
+    while select == "A":
+        word = input('hiragana:')
+        if word == 'quit':
+            break
+        tran = input('translation:')
+        if tran == 'quit':
+            break
+        with open('漢字と意味.txt','a',encoding='utf8') as file:
+            file.write('\n'+word+' '+tran)
+        print('successful')
+
+    if select == 'L':
+        print('=================================')
+        with open('漢字と意味.txt','r',encoding='utf8') as file:
+            print(file.read())
+        print('=================================')
+        
+
+exit()
